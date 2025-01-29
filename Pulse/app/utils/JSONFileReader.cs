9152ca106759c0a2af72dc336885acc1f23211ac
SillyCatto <raiyanmuhtasim@gmail.com>
2025-01-29 20:41:45 +0600

    implement read method for json

---
+++ Pulse/app/utils/JSONFileReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Pulse.utils;
+
+public class JSONFileReader : IFileReader
+{
+    private readonly string _filePath;
+
+    public JSONFileReader(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+
+    public Dictionary<string, object>? Read()
+    {
+        if (!File.Exists(_filePath))
+        {
+            return new Dictionary<string, object>(); //return empty dict
+        }
+
+        string json = File.ReadAllText(_filePath);
+        return JsonSerializer.Deserialize<Dictionary<string, object>>(json) ?? new Dictionary<string, object>();
+    }
+
+    public object? GetValue(string keyPath)
+    {
+        throw new NotImplementedException();
+    }
+}
