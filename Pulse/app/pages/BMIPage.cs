de264950d7a7bd0f3765149027b9974f56ef2608
SillyCatto <raiyanmuhtasim@gmail.com>
2025-02-07 23:57:04 +0600

    added delete record option

--- Pulse/app/pages/BMIPage.cs
+++ Pulse/app/pages/BMIPage.cs
@@ -27,6 +27,7 @@ public class BMIPage : IPageAdapter
     private void RegisterChoices()
     {
         _choiceManager.Add("Add", () => new AddBMIRecord(this));
+        _choiceManager.Add("Delete", () => new DeleteBMIRecord(this));
         _choiceManager.Add("Back", () => new ChoiceBackToHome(_pageManager));
         _choiceManager.Add("Exit", () => new ChoiceExit());
     }
---
+++ Pulse/app/pages/choices/BMIPageChoice/DeleteBMIRecord.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Pulse.utils;
+using Spectre.Console;
+
+namespace Pulse.app.pages.choices.BMIPageChoice;
+
+public class DeleteBMIRecord : IChoiceAdapter
+{
+    private readonly BMIPage _page;
+
+    public DeleteBMIRecord(BMIPage page)
+    {
+        _page = page;
+    }
+    public void Exec()
+    {
+        var bmiRecords = _page.GetDataModel();
+        string date = AskDateInput();
+        
+        if (!bmiRecords.ToDict().ContainsKey(date))
+        {
+            _page.SetMsg(":cross_mark: [magenta2_1]That record doesn't exist[/]");
+            _page.Run();
+            return;
+        }
+
+        bmiRecords.RemoveRecord(date);
+        bmiRecords.Save();
+        _page.SetMsg($":check_mark:  [green]Record for {date} deleted successfully![/]");
+        _page.Run();
+    }
+
+    private static string AskDateInput()
+    {
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>("Enter the date of the record to [bold mediumorchid1_1]delete[/] (yyyy-MM-dd):")
+                .PromptStyle(Constants.PromptInputStyle)
+                .ValidationErrorMessage("[red]Wrong date format, try again: (yyyy-MM-dd)[/]")
+                .Validate(input =>
+                    DateTime.TryParseExact(input, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _)
+                        ? ValidationResult.Success()
+                        : ValidationResult.Error("[red]Invalid date format. Use yyyy-MM-dd.[/]"))
+        );
+    }
+}
